a27e5037b064d23cb99739189823dc97456cbe68
frankcouchman-bit <frankcouchman@gmail.com>
2025-10-17 22:19:13 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ import ArticleWriter from './pages/ArticleWriter'
 import WritingTool from './pages/WritingTool'
 import Privacy from './pages/Privacy'
 import Terms from './pages/Terms'
+import Success from './pages/Success'
+import Blog from './pages/Blog'
+import NotFound from './pages/NotFound'
 
 function AuthCallbackHandler() {
   const { setAuth } = useAuth()
@@ -51,12 +54,15 @@ function App() {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/tools" element={<Tools />} />
             <Route path="/article/:id" element={<Article />} />
+            <Route path="/article/new" element={<Article />} />
             <Route path="/ai-writer" element={<AIWriter />} />
             <Route path="/article-writer" element={<ArticleWriter />} />
             <Route path="/writing-tool" element={<WritingTool />} />
             <Route path="/privacy" element={<Privacy />} />
             <Route path="/terms" element={<Terms />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
