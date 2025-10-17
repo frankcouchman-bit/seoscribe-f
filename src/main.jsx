05801323130715cd2a01ba536e8a7fe066a0f146
frankcouchman-bit <frankcouchman@gmail.com>
2025-10-17 19:20:44 +0200

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/globals.css'
 import { Toaster } from 'react-hot-toast'
+import ErrorBoundary from './components/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <Toaster 
-      position="top-right"
-      toastOptions={{
-        duration: 3000,
-        style: {
-          background: '#1e293b',
-          color: '#fff',
-          border: '1px solid rgba(255,255,255,0.1)'
-        }
-      }}
-    />
+    <ErrorBoundary>
+      <App />
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: '#1e293b',
+            color: '#fff',
+            border: '1px solid rgba(255,255,255,0.1)'
+          }
+        }}
+      />
+    </ErrorBoundary>
   </React.StrictMode>,
 )
